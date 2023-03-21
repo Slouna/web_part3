6727d5ffb34da00ce9948762d387f2c99a8a567b
Santeri Louna <santeri.louna@hotmail.fi>
2023-03-21 13:21:23 +0200

    3.1 ok

--- index.js
+++ index.js
@@ -1,8 +1,31 @@
 const http = require('http')
 
+let persons = [
+    {
+      name: "Arto Hellas",
+      number: "040-123456",
+      id: 1
+    },
+    {
+      name: "Ada Lovelace",
+      number: "39-44-5323523",
+      id: 2
+    },
+    {
+      name: "Dan Abramov",
+      number: "12-43-234345",
+      id: 3
+    },
+    {
+      name: "Mary Poppendieck",
+      number: "39-23-6423122",
+      id: 4
+    }
+  ]
+
 const app = http.createServer((request, response) => {
-  response.writeHead(200, { 'Content-Type': 'text/plain' })
-  response.end('Hello World')
+  response.writeHead(200, { 'Content-Type': 'application/json' })
+  response.end(JSON.stringify(persons))
 })
 
 const PORT = 3001
